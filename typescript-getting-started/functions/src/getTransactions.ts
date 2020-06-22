2bdb83bd77e3b33b21a6433da2cf1c45d39a1692
Henrique Duarte <henriqueduarte41@hotmail.com>
2020-06-22 16:38:50 +0200

    Update getTransactions.ts

--- typescript-getting-started/functions/src/getTransactions.ts
+++ typescript-getting-started/functions/src/getTransactions.ts
@@ -1,15 +1,12 @@
+
 let transaction_01 = new Transaction("001", "first_transaction", "1st_Description", "Sucess");
 let transaction_02 = new Transaction("002", "second_transaction", "2nd_Description", "Sucess");
 let transaction_03 = new Transaction("003", "third_transaction", "3rd_Description", "Sucess");
 
 
-function getTransactions(userId: string, fromDate: number, toDate: number){
+function getTransactions(userId: string, fromDate: number, toDate: number) {
+    
     
     return userId.toString;
 }
 
-
-
-
-
-getTransactions.call;
